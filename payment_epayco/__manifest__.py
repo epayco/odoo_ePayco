fd340fe3daa7f29cb25a2a01f63c8ee50c2e172e
pruebasepayco <40249987+pruebasepayco@users.noreply.github.com>
2021-04-30 22:31:33 -0500

    Update __manifest__.py

--- payment_epayco/__manifest__.py
+++ payment_epayco/__manifest__.py
@@ -1,19 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
     'name': 'Epayco Payment Acquirer',
     'category': 'Accounting/Payment',
     'summary': 'Payment Acquirer: Epayco Implementation',
-    'description': """
-    Epayco Payment Acquirer for India.
-    Epayco payment gateway supports only INR currency.
-    """,
-    'sequence': 365,
-    'summary': 'Payment Acquirer: Epayco Implementation',
-    'version': '1.0',
     'description': """Epayco Payment Acquirer""",
     'depends': ['payment'],
     'data': [
@@ -22,8 +13,4 @@
         'data/payment_acquirer_data.xml',
     ],
     'post_init_hook': 'create_missing_journal_for_acquirers',
-    'installable': True,
-    'application': True,
-    'post_init_hook': 'create_missing_journal_for_acquirers',
-    'uninstall_hook': 'uninstall_hook',
 }
