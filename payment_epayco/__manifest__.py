c9a9cbbf3fa0af964eb1bd7c24bd229e0d4d5866
RicardoSaldarriagaPayco <ricardo.saldarriaga@payco.co>
2021-10-28 22:51:32 -0500

    fix: se restructura codigo, corrigiendo la configuracion de instalacion

--- payment_epayco/__manifest__.py
+++ payment_epayco/__manifest__.py
@@ -1,29 +1,18 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Epayco Payment Acquirer',
-    'category': 'Accounting/Payment',
-    'summary': 'Payment Acquirer: Epayco Implementation',
-    'description': """
-    Epayco Payment Acquirer for India.
-    Epayco payment gateway supports only INR currency.
-    """,
-    'sequence': 365,
-    'summary': 'Payment Acquirer: Epayco Implementation',
-    'version': '1.0',
-    'description': """Epayco Payment Acquirer""",
+    'name': 'Payco Payment Acquirer',
+    'category': 'Payment Acquirer',
+    'summary': 'Payment Acquirer: Payco Implementation',
+    'description': """ePayco payment gateway.""",
     'depends': ['payment'],
     'data': [
         'views/payment_views.xml',
-        'views/payment_epayco_templates.xml',
+        'views/payment_payco_templates.xml',
         'data/payment_acquirer_data.xml',
     ],
     'post_init_hook': 'create_missing_journal_for_acquirers',
-    'installable': True,
-    'application': True,
-    'post_init_hook': 'create_missing_journal_for_acquirers',
     'uninstall_hook': 'uninstall_hook',
+    'license': 'LGPL-3',
 }
