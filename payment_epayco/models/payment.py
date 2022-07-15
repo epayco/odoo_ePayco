52d7b6b48f61c7a47656ebab414f79ea5d3ce742
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-07-15 11:25:56 -0500

    feat:actualizacion modulo 13

--- payment_epayco/models/payment.py
+++ payment_epayco/models/payment.py
@@ -1,56 +1,93 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from builtins import print
 import hashlib
+
+import hashlib
+from re import S
 import uuid
 from werkzeug import urls
 
 from odoo import api, fields, models, _
 from odoo.addons.payment.models.payment_acquirer import ValidationError
 from odoo.tools.float_utils import float_compare
+
 import logging
 
 _logger = logging.getLogger(__name__)
 
-class PaymentAcquirerEpayco(models.Model):
-    _inherit = 'payment.acquirer'
-    provider = fields.Selection(selection_add=[('epayco', 'Epayco')])
 
-    epayco_p_cust_id = fields.Char(string='P_CUST_ID', required_if_provider='epayco', groups='base.group_user')
-    epayco_p_key = fields.Char(string='P_KEY', required_if_provider='epayco', groups='base.group_user')
-    epayco_public_key = fields.Char(string='PUBLICK_KEY', required_if_provider='epayco', groups='base.group_user')
-    epayco_checkout_type = fields.Selection(
+class PaymentAcquirerPayco(models.Model):
+    _inherit = 'payment.acquirer'
+
+    provider = fields.Selection(selection_add=[('payco', 'Payco')])
+    payco_merchant_key = fields.Char(string='P_CUST_ID_CLIENTE', required_if_provider='payco', groups='base.group_user')
+    payco_merchant_salt = fields.Char(string='PUBLIC_KEY', required_if_provider='payco', groups='base.group_user')
+    payco_p_keyt = fields.Char(string='P_KEY', required_if_provider='payco', groups='base.group_user')
+    payco_checkout_type = fields.Selection(
         selection=[('onpage', 'Onpage Checkout'),
                    ('standard', 'Standard Checkout')],
-        required_if_provider='epayco',
+        required_if_provider='payco',
         string='Checkout Type',
         default='onpage')
 
-    def epayco_form_generate_values(self, values):
+    def _get_payco_urls(self):
+        """ Payco URLs"""
         self.ensure_one()
-        env_test = 'false' if self.environment == 'prod' else 'true'
+        base_url = self.get_base_url()
+        return {'payco_form_url': urls.url_join(base_url,  '/payment/payco/checkout')}
+
+    def _payco_generate_sign(self,  signature, values):
+        """ Generate the signature for incoming communications.
+        """
+        p_cust_id_client = self.payco_merchant_key
+        p_key = self.payco_p_keyt
+        x_ref_payco = values.get('x_ref_payco')
+        x_transaction_id = values.get('x_transaction_id')
+        x_amount = values.get('x_amount')
+        x_currency_code = values.get('x_currency_code')
+        hash_str_bytes = bytes('%s^%s^%s^%s^%s^%s' % (
+            p_cust_id_client,
+            p_key,
+            x_ref_payco,
+            x_transaction_id,
+            x_amount,
+            x_currency_code), 'utf-8')
+        hash_object = hashlib.sha256(hash_str_bytes)
+        hash = hash_object.hexdigest()
+
+        if hash == signature:
+            shasign = True
+        else:
+            shasign = False
+
+        return shasign
+
+    def payco_form_generate_values(self, values):
+        self.ensure_one()
+        base_url = self.get_base_url()
         partner_lang = values.get('partner') and values['partner'].lang
+        test = 'false' if self.state == 'enabled' else 'true'
         lang = 'es' if 'es' in partner_lang else 'en'
         country = values.get('partner_country').code.lower()
-        epayco_checkout_external = (
-            'false' if self.epayco_checkout_type == 'onpage' else 'true')
+        payco_checkout_external = (
+            'false' if self.payco_checkout_type == 'onpage' else 'true')
         tx = self.env['payment.transaction'].search([('reference', '=', values.get('reference'))])
         if tx.state not in ['done', 'pending']:
             tx.reference = str(uuid.uuid4())
-        base_url = self.get_base_url()
-        url_confirmation = urls.url_join(base_url, '/payment/epayco/confirmation/')
-        url_response = urls.url_join(base_url, '/payment/epayco/response/')
-        epayco_tx_values = dict(values)
-        split_reference = epayco_tx_values.get('reference').split('-')
-        order = ''
+        url_confirmation = urls.url_join(base_url, '/payment/payco/confirmation/')
+        url_response = urls.url_join(base_url, '/payment/payco/response/')
+        payco_tx_values = dict(values)
+        split_reference = payco_tx_values.get('reference').split('-')
         order = ''
         tax = values['partner'].last_website_so_id.amount_tax
         base_tax = values['partner'].last_website_so_id.amount_undiscounted
         amount = values['partner'].last_website_so_id.amount_total
         if split_reference:
             order = split_reference[0]
-        epayco_tx_values.update({
-            'public_key': self.epayco_public_key,
+        payco_tx_values.update({
+            'public_key': self.payco_merchant_salt,
             'txnid': order,
             'amount': amount,
             'tax': tax,
@@ -61,96 +98,71 @@ class PaymentAcquirerEpayco(models.Model):
             'phone': values.get('partner_phone'),
             'currency_code': values['currency'] and values['currency'].name or '',
             'country_code': country,
-            'epayco_checkout_external': epayco_checkout_external,
-            'epayco_env_test': env_test,
+            'epayco_checkout_external': payco_checkout_external,
+            'epayco_env_test': test,
             'epayco_lang': lang,
             'response_url': url_response,
             'url_confirmation': url_confirmation,
-            'extra1': env_test,
+            'extra1': order,
             'extra2': values['reference'],
+            'extra3': test
         })
-        return epayco_tx_values
+        return payco_tx_values
 
-    def epayco_get_form_action_url(self):
+    def payco_get_form_action_url(self):
         self.ensure_one()
-        return '/payment/epayco/checkout/'
+        return self._get_payco_urls()['payco_form_url']
 
 
-    def _epayco_generate_sign(self, values):
-        """ Generate the shasign for incoming or outgoing communications.
-        :param self: the self browse record. It should have a shakey in shakey out
-        :param string inout: 'in' (odoo contacting epayco) or 'out' (epayco
-                             contacting odoo).
-        :param dict values: transaction values
-
-        :return string: shasign
-        """
-        self.ensure_one()
-        p_key = self.epayco_p_key
-        x_ref_payco = values.get('x_ref_payco')
-        x_transaction_id = values.get('x_transaction_id')
-        x_amount = values.get('x_amount')
-        x_currency_code = values.get('x_currency_code')
-        hash_str_bytes = bytes('%s^%s^%s^%s^%s^%s' % (
-            self.epayco_p_cust_id,
-            p_key,
-            x_ref_payco,
-            x_transaction_id,
-            x_amount,
-            x_currency_code), 'utf-8')
-        hash_object = hashlib.sha256(hash_str_bytes)
-        hash = hash_object.hexdigest()
-        return hash
-
-
-class PaymentTransactionEpayco(models.Model):
+class PaymentTransactionPayco(models.Model):
     _inherit = 'payment.transaction'
 
     @api.model
-    def _epayco_form_get_tx_from_data(self, data):
-        """ Given a data dict coming from epayco, verify it and find the related
+    def _payco_form_get_tx_from_data(self, data):
+        """ Given a data dict coming from ePayco, verify it and find the related
         transaction record. """
-        reference = data.get('x_extra2')
+        reference = data.get('x_extra1')
+        pay_id = data.get('x_extra2')
         signature = data.get('x_signature')
-        if not reference or not reference or not signature:
-            raise ValidationError(_('Epayco: received data with missing reference (%s) or signature (%s)') % (reference, signature))
 
-        transaction = self.search([('reference', '=', reference)])
+        if not reference or not pay_id or not signature:
+            raise ValidationError(_('ePayco: received data with missing reference (%s) or pay_id (%s) or shashign (%s)') % (reference, pay_id, signature))
 
+        transaction = self.search([('reference', '=', pay_id)])
         if not transaction:
-            error_msg = (_('Epayco: received data for reference %s; no order found') % (reference))
+            error_msg = (_('Payco: received data for reference %s; no order found') % (reference))
             raise ValidationError(error_msg)
         elif len(transaction) > 1:
-            error_msg = (_('Epayco: received data for reference %s; multiple orders found') % (reference))
-            raise ValidationError(error_msg)        
+            error_msg = (_('Payco: received data for reference %s; multiple orders found') % (reference))
+            raise ValidationError(error_msg)
 
         return transaction
 
-    def _epayco_form_get_invalid_parameters(self, data):
+    def _payco_form_get_invalid_parameters(self, data):
         invalid_parameters = []
-            # check what is buyed
-        if int(self.acquirer_id.epayco_p_cust_id) != int(data.get('x_cust_id_cliente')):
+        #check what is buyed
+        if int(self.acquirer_id.payco_merchant_key) != int(data.get('x_cust_id_cliente')):
             invalid_parameters.append(
-                ('Customer ID', data.get('x_cust_id_cliente'), self.acquirer_id.epayco_p_cust_id))
+                ('Custore_id', data.get('x_cust_id_cliente'), self.acquirer_id.payco_merchant_key))
+
         return invalid_parameters
 
-    def _epayco_form_validate(self, data):
+    def _payco_form_validate(self, data):
         cod_response = int(data.get('x_cod_response'))
         result = self.write({
             'acquirer_reference': data.get('x_ref_payco'),
             'date': fields.Datetime.now(),
         })
-        # verify signature
-        reference = data.get('x_extra2')
         signature = data.get('x_signature')
-        shasign_check = self.acquirer_id._epayco_generate_sign(data)
-        tx = self.env['payment.transaction'].search([('reference', '=', reference)])
+        tx = self.env['payment.transaction'].search([('reference', '=', data.get('x_extra2'))])
+        shasign_check = self.acquirer_id._payco_generate_sign(signature, data)
         x_test_request = data.get('x_test_request')
-        isTestPluginMode = 'yes' if data.get('x_extra1') == 'true' else 'no'
+        isTestPluginMode = 'yes' if data.get('x_extra4') == 'true' else 'no'
         x_approval_code = data.get('x_approval_code')
         x_cod_transaction_state = data.get('x_cod_transaction_state')
         isTestTransaction = 'yes' if x_test_request == 'TRUE' else 'no'
         isTestMode = 'true' if isTestTransaction == 'yes' else 'false'
+
         validation = False
         if float_compare(float(data.get('x_amount', '0.0')), tx.amount, 2) == 0:
             if isTestPluginMode == "yes":
@@ -165,11 +177,11 @@ class PaymentTransactionEpayco(models.Model):
                         validation = False
 
         massage = ""
-        if signature == shasign_check and validation == True:
+        if shasign_check == True and validation == True:
             if tx.state not in ['draft']:
                 if cod_response not in [1,3]:
                     allowed_states = ('draft', 'authorized', 'pending')
-                    target_state = 'cancel'
+                    target_state = 'draft'
                     (tx_to_process, tx_already_processed, tx_wrong_state) = self._filter_transaction_state(allowed_states, target_state)
                     massage = "second confirm ePayco"
                     tx_to_process.write({
@@ -177,22 +189,28 @@ class PaymentTransactionEpayco(models.Model):
                         'date': fields.Datetime.now(),
                         'state_message': massage,
                     })
-                    self.manage_status_order(data.get('x_description'),'sale_order')
+                    tx_to_process.write({
+                        'state': 'cancel',
+                        'date': fields.Datetime.now(),
+                        'state_message': massage,
+                    })
+                    self.manage_status_order(data.get('x_extra1'),'sale_order')
                 else:
                     if tx.state in ['pending']:
                         if cod_response == 1:
-                            self._set_transaction_done()    
+                            self._set_transaction_done()
+
             else:
                 if cod_response == 1:
                     self._set_transaction_done()
                 elif cod_response == 3:
                     self._set_transaction_pending()
                 else:
-                    self.manage_status_order(data.get('x_description'),'sale_order')
+                    self.manage_status_order(data.get('x_extra1'),'sale_order')          
         else:
             if tx.state in ['done']:
                 allowed_states = ('draft', 'authorized', 'pending','done')
-                target_state = 'cancel'
+                target_state = 'draft'
                 (tx_to_process, tx_already_processed, tx_wrong_state) = self._filter_transaction_state(allowed_states, target_state)
                 massage = "second confirm ePayco"
                 tx_to_process.write({
@@ -200,12 +218,17 @@ class PaymentTransactionEpayco(models.Model):
                     'date': fields.Datetime.now(),
                     'state_message': massage,
                 })
-                self.manage_status_order(data.get('x_description'),'stock_picking', confirmation=True)
-                self.manage_status_order(data.get('x_description'),'sale_order')
-                self.manage_status_order(data.get('x_description'),'stock_move', confirmation=True)
-            else: 
-                self.manage_status_order(data.get('x_description'),'sale_order')
-
+                tx_to_process.write({
+                    'state': 'cancel',
+                    'date': fields.Datetime.now(),
+                    'state_message': massage,
+                })
+                self.manage_status_order(data.get('x_extra1'),'stock_picking', confirmation=True)
+                self.manage_status_order(data.get('x_extra1'),'sale_order')
+                self.manage_status_order(data.get('x_extra1'),'stock_move', confirmation=True)
+            else:
+                self.manage_status_order(data.get('x_extra1'),'sale_order')
+   
         return result
 
     def query_update_status(self, table, values, selectors):
