f59db3a1caac6eb0509738493afc55babc386133
JuanEstebanGarciaV <juan.garcia@epayco.com>
2025-04-19 21:29:08 -0500

    Renombrar banner para que odoo lo pueda reconocer

--- payment_epayco/__manifest__.py
+++ payment_epayco/__manifest__.py
@@ -13,7 +13,7 @@
         'views/payment_epayco_templates.xml',
         'data/payment_acquirer_data.xml',
     ],
-    'images': [ 'static/images/screen_image.png'],
+    'images': [ 'static/images/main_screenshot.png'],
     'application': True,
     'post_init_hook': 'create_missing_journal_for_acquirers',
     'uninstall_hook': 'uninstall_hook',
